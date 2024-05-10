34dd2f59a13d89c8cfa447e462ffcff498c0aa83
ss-zun <zxq0158@gmail.com>
2024-05-10 23:48:54 +0900

    feature : 캐릭터 애니메이션 추가
    AnimationController.cs : 애니메이션 컨트롤러
    CharacterAnimationController.cs : 캐릭터 애니메이션 처리
    isWalking(bool) 로 idle상태에서 이동상태일 때 walk 애니메이션 전환

---
+++ Assets/Scripts/Entites/Controllers/CharacterAnimationController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class CharacterAnimationController : AnimationController
+{
+    private static readonly int isWalking = Animator.StringToHash("isWalking");
+
+    // walking을 하는데 너무 조금 움직이고 있으면 그냥 멈춘걸로 처리(Threshold : 문턱)
+    // 0.5는 넘어야한다는 의미로 쓴 것
+    private readonly float magnituteThreshold = 0.5f; 
+
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+    private void Start()
+    {
+        controller.OnMoveEvent += Move;
+    }
+
+    private void Move(Vector2 vector)
+    {
+        // 0.5보다 작으면 false, 크면 true
+        animator.SetBool(isWalking, vector.magnitude > magnituteThreshold);
+    }
+}
